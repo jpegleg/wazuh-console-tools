c338e6fe6519c42affa5b077f9da6845cd01ce5c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-10-16 21:35:53 -0600

    Update console_screen_5.sh

--- console_screen_5.sh
+++ console_screen_5.sh
@@ -7,7 +7,7 @@ echo
 sleep 2
 
 cat /var/ossec/logs/alerts/alerts.json | grep "level..7" | while read line; do 
-    echo $line | grep -oE 'host.{28}|name.{28}' | cut -d':' -f1-2; echo $line | grep -oE 'path.{480}'; sleep 10;
+    echo $line | grep -oE 'host.{32}|name.{32}' | cut -d':' -f1-2; echo $line | grep -oE 'path.{480}' | rev | cut -d':' -f2- | rev; sleep 10;
 done
 
 echo
@@ -16,5 +16,5 @@ echo
 sleep 2
 
 cat /var/ossec/logs/alerts/alerts.json | grep "level..14" | while read line; do
-    echo $line | grep -oE 'host.{28}|name.{28}' | cut -d':' -f1-2; echo $line | grep -oE 'path.{480}'; sleep 10;
+    echo $line | grep -oE 'host.{32}|name.{32}' | cut -d':' -f1-2; echo $line | grep -oE 'path.{480}' | rev | cut -d':' -f2- | rev; sleep 10;
 done
